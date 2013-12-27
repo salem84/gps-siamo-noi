48366bc44db897d3b6a063d7de82c5c797d2968a
salem84 <salem84@hotmail.it>
2013-12-28 00:57:46 +0100

    modifica config

---
+++ utils/twitter_config.js
@@ -0,0 +1,8 @@
+var twit = require('twit')
+
+module.exports = new twit({
+    consumer_key: '21sQGTwFubvAIrvfMeIZQ',
+    consumer_secret: 'QswEDhRYrwzWa3nLNfyqMZ0tg4kgPYK7Jvv16HlLemw',
+    access_token: '165020788-89DKT8UYsioK32jsp3poB58szKRmh9BoU8HtJzGR',
+    access_token_secret: 'sKwdzOFDBUHaSVV7ouf6qf4Zzry1f8tuv3H1bxJBEiuRl'
+});
