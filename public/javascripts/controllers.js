f334206279145c5e2503c7e77ea042def0e0a3ed
salem84 <salem84@hotmail.it>
2013-12-28 17:04:17 +0100

    aggiunta paginazione
    Related Work Items: #8

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -86,7 +86,16 @@ function TweetsProfiloCtrl($scope, $routeParams, Tweets) {
 	$scope.profilo =  $routeParams.profilo;
 	$scope.tweets = Tweets.query({screenName: $routeParams.profilo});
 
-    $scope.lastId = function() {
+    $scope.cmdPreviousTweets = function() {
+        var lastId = getLastId();
+       Tweets.query({ screenName: $routeParams.profilo, maxId: lastId }, function(result) {
+           $scope.tweets=$scope.tweets.concat(result);
+       });
+        
+    };
+
+    //$scope.lastId = function() {
+    function getLastId() {
         var len = $scope.tweets.length;
         if (len > 0)
             return $scope.tweets[len - 1].id;
