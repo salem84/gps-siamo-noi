c877848d019e487bcee441cef003472e5f8fa81d
salem84 <salem84@hotmail.it>
2014-01-02 18:57:48 +0100

    aggiunti file

---
+++ client/scripts/controllers/c_contattaci.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+
+app.controller('ContattaciCtrl', function($scope, Assistenza) {
+    $scope.motiviSegnalazione = ['Ho un problema', 'Aggiunta funzionalità', 'Altro...'];
+
+    $scope.selectedMotivoSegnalazione = undefined;
+    $scope.descrizione = undefined;
+    $scope.email = undefined;
+    
+    $scope.cmdSelezionaMotivo = function(index) {
+        $scope.selectedMotivoSegnalazione = $scope.motiviSegnalazione[index];
+
+    };
+
+    $scope.isValid = function() {
+        return selectedMotivoSegnalazione && descrizione && email;
+    };
+
+    $scope.emailInviata = undefined;
+
+    $scope.cmdInvia = function() {
+        Assistenza.post({ motivo: $scope.selectedMotivoSegnalazione, descrizione: $scope.descrizione, email: $scope.email },
+        function(result) {
+            
+        });
+    };
+});
