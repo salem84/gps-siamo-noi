abc7507b9c3e0e680aa109a75e9a4263640bc82f
giorgio.lasala <giorgio.lasala@gmail.com>
2014-12-28 20:39:30 +0100

    aggiornamento express 4 e altri pacchetti

--- app.js
+++ app.js
@@ -4,11 +4,21 @@
  */
 
 var express = require('express'),
-    http = require('http'),
+    //Necessari da upgrade da Express 3
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    bodyParser = require('body-parser'),
+    multer = require('multer'),
+    errorHandler = require('errorhandler'),
+    cookieParser = require('cookie-parser'),
+
+
     path = require('path'),
     fs = require('fs'),
     passport = require('passport'),
-    MongoStore  = require('connect-mongo')(express),
+    MongoStore = require('connect-mongo')(session),
     routes = require('./server/routes'),
     auth = require('./server/utils/authentication'),
     log = require('./server/logger'),
@@ -23,12 +33,12 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
+app.use(favicon(__dirname + '/client/favicon.png'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride());
+app.use(cookieParser());
 //app.use(express.session({
 //     secret: process.env.COOKIE_SECRET || 'gpssiamonoi_coookie_secret'
 //}));
@@ -37,7 +47,7 @@ app.use(express.cookieParser());
 //         secret: process.env.COOKIE_SECRET || 'gpssiamonoi_coookie_secret'
 //     }));
 
-app.use(express.session({
+app.use(session({
   secret  : config.cookie_secret,
   cookie  : {
     maxAge  : 7 * 24 * 60 * 60 * 1000              // expire the session(-cookie) after # seconds
@@ -57,7 +67,7 @@ app.use(express.static(path.join(__dirname, 'client')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorHandler());
 }
 
 
@@ -70,7 +80,7 @@ passport.deserializeUser(auth.deserializeUser);
 require('./server/routes.js')(app);
 
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   log.info('Express server listening on port ' + app.get('port'));
 });
 
